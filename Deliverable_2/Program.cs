89b650037bbbe5d22aef6140bd0a2b5587a8e6b9
Gabrielle <Gabrielle@LAPTOP-H0ABB48F>
2023-01-24 17:19:28 -0500

    Add project files.

---
+++ Deliverable_2/Program.cs
@@ -0,0 +1,54 @@
+﻿namespace Deliverable_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, please input the grade you expect to get in ISM 4300:");
+            try
+            {
+                decimal classGrade = decimal.Parse(Console.ReadLine()); ;
+                string letterGrade;
+
+                if ((classGrade > 89) && (classGrade < 100))
+                {
+                    letterGrade = "A";
+                    Console.WriteLine("Your expected letter grade is: " + letterGrade);
+                    Console.ReadKey(true);
+                }
+                else if ((classGrade > 80) && (classGrade < 90))
+                {
+                    letterGrade = "B";
+                    Console.WriteLine("Your expected letter grade is: " + letterGrade);
+                    Console.ReadKey(true);
+                }
+                else if ((classGrade > 70) && (classGrade < 80))
+                {
+                    letterGrade = "C";
+                    Console.WriteLine("Your expected letter grade is: " + letterGrade);
+                    Console.ReadKey(true);
+                }
+                else if ((classGrade > 60) && (classGrade < 70))
+                {
+                    letterGrade = "D";
+                    Console.WriteLine("Your expected letter grade is: " + letterGrade);
+                    Console.ReadKey(true);
+                }
+                else if ((classGrade < 60) && (classGrade > 0))
+                {
+                    letterGrade = "F";
+                    Console.WriteLine("Your expected letter grade is: " + letterGrade);
+                    Console.ReadKey(true);
+                }
+                else if (classGrade < 0)
+                {
+                    Console.WriteLine("Please input a number greater than 0");
+                }
+            }
+            catch
+            {
+                Console.WriteLine("Please enter a decimal number");
+            }
+        }
+    }
+}
